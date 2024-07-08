4096a5494c395f82c32e6f8d68738a392685d892
Lucas Gonzalez <lucas9set99@gmail.com>
2024-07-07 22:24:34 -0300

    main.tf modifi

--- iac/main.tf
+++ iac/main.tf
@@ -9,18 +9,20 @@ terraform {
   }
 }
 
-# Provider Block
+# Variables para las credenciales de AWS
 variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS session token"
+  description = "AWS access key"
   type        = string
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS session token"
-  type        = string
-}
-provider "aws" {
-  region  = "us-east-1"
-  access_key = AWS_ACCESS_KEY_ID
-  secret_key = AWS_SECRET_ACCESS_KEY
 }
 
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+  type        = string
+}
+
+# Provider Block
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
